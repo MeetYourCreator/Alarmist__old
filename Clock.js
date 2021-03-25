94b438935e81541848fa8b461284122dc06563c4
MeetYourCreator <ramizackshamir@icloud.com>
2021-03-25 13:02:20 -0400

    init Styled Components

--- Clock.js
+++ Clock.js
@@ -1,15 +1,32 @@
 import { StyleSheet, View, Text, SafeAreaView } from 'react-native'
-import styled from 'styled-components'
+import styled from 'styled-components/native'
 
-export const ClockFace = styled.View`
+export const Container = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: rgb(238,235,235);
+  overflow: hidden;
+`;
+
+const ClockEdge = styled.View`
+  background-color:rgba(255, 0, 85, 0.485);
+  border-top-right-radius: 250px;
+  border-bottom-right-radius: 250px;
+  border-top-left-radius: 40px;
+  border-bottom-left-radius: 50px;
+  height: 700px;
+`
+const ClockFace = styled.View`
   width: 500px;
   height: 500px;
   background-color: rgba(102, 193, 230, 0.485);
   border-radius: 50%;
   border: 3px solid white;
   box-shadow:  -8px 8px 15px rgba(225,225,225,0.05),
-       20px 20px 20px rgba(0,0,0,0.2),
-       inset -8px -8px 15px rgba(225,225,225,0.05),
-       inset 20px 20px 20px rgba(0,0,0,0.2);
+    20px 20px 20px rgba(0,0,0,0.2),
+    inset -8px -8px 15px rgba(225,225,225,0.05),
+    inset 20px 20px 20px rgba(0,0,0,0.2);
   position: relative;
 `
--- Clock.jsx
+++ Clock.jsx
@@ -1,7 +1,33 @@
-import React, { useState, useEffect } from 'react'
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import { setHour, setMinute, setSecond } from './time.js'
+import React, { useState, useEffect } from "react"
+import { StyleSheet, Text, View, SafeAreaView } from "react-native"
+import { setHour, setMinute, setSecond } from "./time.js"
+import styled from 'styled-components/native'
 
+const Container = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100px;
+  background-color: rgb(238,235,235);
+  overflow: hidden;
+`
+
+const ClockEdge = styled.View`
+  background-color:rgba(255, 0, 85, 0.485);
+  border-top-right-radius: 250px;
+  border-bottom-right-radius: 250px;
+  border-top-left-radius: 40px;
+  border-bottom-left-radius: 50px;
+  height: 700px;
+`
+const ClockFace = styled.View`
+  width: 375px;
+  height: 375px;
+  background-color: rgba(102, 193, 230, 0.485);
+  border-radius: 175px;
+  border: 3px solid white;
+  position: relative;
+`
 
 const Clock = () => {
   const [hourRatio, setHourRatio] = useState(0)
@@ -16,74 +42,42 @@ const Clock = () => {
     }, 1000)
   }, [])
 
-
-
+  
   return (
     <>
-      <SafeAreaView>
-        <View style={styles.clock}>
-        <View style={styles.clockFace}>
-        <Text style={styles.number1}>
-          1
-        </Text>
-        <Text>
-          2
-        </Text>
-        <Text>
-          3
-        </Text>
-        <Text>
-          4
-        </Text>
-        <Text>
-          5
-        </Text>
-        <Text>
-          6
-        </Text>
-        <Text>
-          7
-        </Text>
-        <Text>
-          8
-        </Text>
-        <Text>
-          9
-        </Text>
-        <Text>
-          10
-        </Text>
-        <Text>
-          11
-        </Text>
-        <Text>
-          12
-        </Text>
-        </View>
-       </View>
-      </SafeAreaView>
-            
+      <Container>
+        {/* <View style={styles.clock}> */}
+          <ClockFace>
+            <Text>1</Text>
+            <Text>2</Text>
+            <Text>3</Text>
+            <Text>4</Text>
+            <Text>5</Text>
+            <Text>6</Text>
+            <Text>7</Text>
+            <Text>8</Text>
+            <Text>9</Text>
+            <Text>10</Text>
+            <Text>11</Text>
+            <Text>12</Text>
+          </ClockFace>
+        {/* </View> */}
+      </Container>
     </>
   )
 }
 
-const styles = StyleSheet.create({
-  clock: {
-  },
+// const styles = StyleSheet.create({
+//   clock: {},
 
-  clockFace: {
-    width: 375,
-    height: 375,
-    backgroundColor: '#66c1e67c',
-    borderRadius: 200,
-    borderColor: 'black',
-    borderWidth: 3,
-    position: 'relative'
-  },
-
-  number1: {
-    transform: 'rotate(-30deg)',
-  }
-
-})
-export default Clock;
+//   clockFace: {
+//     width: 375,
+//     height: 375,
+//     backgroundColor: "#66c1e67c",
+//     borderRadius: 200,
+//     borderColor: "black",
+//     borderWidth: 3,
+//     position: "relative",
+//   },
+// })
+export default Clock
