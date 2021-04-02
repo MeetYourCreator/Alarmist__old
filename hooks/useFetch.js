86ed76a045a336f54160c25cae77b20dc22f1422
MeetYourCreator <ramizackshamir@icloud.com>
2021-04-02 14:25:55 -0400

    useEffect hook

---
+++ hooks/useFetch.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+
+export const useFetch = (initialState, url) => {
+  const [data, setData] = useState(initialState);
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data)
+      })
+      .catch(error => setError(error))
+      .finally(() => setLoading(false))
+  }, [url]);
+
+  return {
+    data,
+    error,
+    loading
+  }
+}
