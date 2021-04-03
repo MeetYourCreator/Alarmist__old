61e8177032bdb4054ace75edd762bc0cbaddca6d
MeetYourCreator <ramizackshamir@icloud.com>
2021-04-03 13:59:07 -0400

    code CleanUp

--- screens/DigitalClock/LocalDigitalClock.jsx
+++ screens/DigitalClock/LocalDigitalClock.jsx
@@ -29,49 +29,4 @@ const LocalDigitalClock = ({ navigation }) => {
   )
 }
 
-// const LocalClockContainer = styled.View`
-//   display: flex;
-//   flex: 1;
-//   flex-direction: column;
-//   justify-content: center;
-//   align-items: center;
-//   padding: 40px;
-// `
-
-// const Clock = styled.View`
-//   position: relative;
-//   width: 350px;
-//   height: auto;
-//   border: 8px solid #2d2d2d;
-//   border-radius: 15px;
-//   background-color: #151617;
-// `
-
-// const ClockText = styled.Text`
-//   display: flex;
-//   margin: 15px;
-//   text-shadow: 3px 3px 20px #f70300;
-//   justify-content: center;
-//   align-items: center;
-//   text-align: center;
-//   font-size: 65px;
-//   color: rgb(247, 1, 3);
-//   }
-// `
-
-// const Location = styled.Text`
-//   margin-top: 15px;
-//   margin-right: 7px;
-//   margin-bottom: 5px;
-//   margin-left: 7px;
-//   border-top-width: 2px;
-//   text-align: center;
-//   justify-content: center;
-//   align-items: center;
-//   display: flex;
-//   color: #000000;
-//   font-size: 13px;
-//   font-variant: small-caps;
-// `
-
 export default LocalDigitalClock
