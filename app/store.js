8c90bb1b744b47e574848286622433b87728d1d3
MeetYourCreator <ramizackshamir@icloud.com>
2021-04-17 14:52:49 -0400

    init features/colors and features/fontTypes

--- app/store.js
+++ app/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
+import colorsReducer from './features/colors/colorsReducer.js'
+import fontTypesReducer from './features/fontTypes/fontTypesReducer.js'
+
 
 //The Redux store is reated using the configureStore function from Redux Toolkit. configureStoreequires that we pass in a reducer argument.
 
 export default configureStore({
 
   reducer: {
-  
+    colors: colorsReducer,
+    fontTypes: fontTypesReducer
   }
 })
