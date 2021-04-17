d1b2d4b0810d222ed95f024c44198085363bb1a4
MeetYourCreator <ramizackshamir@icloud.com>
2021-04-17 14:47:52 -0400

    init Redux

---
+++ app/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+//The Redux store is reated using the configureStore function from Redux Toolkit. configureStoreequires that we pass in a reducer argument.
+
+export default configureStore({
+
+  reducer: {
+  
+  }
+})
