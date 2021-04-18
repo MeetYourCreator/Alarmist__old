0d41cb8bd47f60326f27ffaae43394fec271ca0e
MeetYourCreator <ramizackshamir@icloud.com>
2021-04-17 21:06:52 -0400

    update colorsReducer

---
+++ app/store2.js
@@ -0,0 +1,32 @@
+{
+  colors: [
+    {
+      color: '#E8E500'
+    },
+    {
+      color: '#FF2281'
+    },
+    {
+      color: '#75D5FD'
+    },
+    {
+      color: '#09FBD3'
+    },
+    {
+      color: '#7122FA'
+    },
+    {
+      color: '#EF281E'
+    },
+    {
+      color: '#0000FF'
+    },
+  ],
+    font: [
+    {
+      fontFamily: 'nosifer'
+    }
+    
+    ]
+
+}
--- features/colors/colorsReducer.js
+++ features/colors/colorsReducer.js
@@ -1,10 +1,55 @@
 import { createStore } from 'redux';
 
-const colorsReducer = (state = { value } , action) => {
-  switch (action.type) {
-    case 'color/updated';
-      return { value: state.value }
-  }
+const colorsReducer = (state, action) => {
+  if (action.type === 'SET_E8E500')
+    return [
+      ...state,
+      {
+        color: '#E8E500'
+      }
+    ];
+  else if (action.type === 'SET_FF2281')
+    return [
+      ...state,
+      {
+        color: '#FF2281'
+      }
+    ];
+  else if (action.type === 'SET_75D5FD')
+    return [
+      ...state,
+      {
+        color: '#75D5FD'
+      }
+    ];
+  else if (action.type === 'SET_09FBD3')
+    return [
+      ...state,
+      {
+        color: '#09FBD3'
+      }
+    ];
+  else if (action.type === 'SET_7122FA')
+    return [
+      ...state,
+      {
+        color: '#7122FA'
+      }
+    ];
+  else if (action.type === 'SET_EF281E')
+    return [
+      ...state,
+      {
+        color: '#EF281E'
+      }
+    ];
+  else if (action.type === 'SET_0000FF')
+    return [
+      ...state,
+      {
+        color: '#0000FF'
+      }
+    ];
 }
 
 //create a Redux store holding the state
