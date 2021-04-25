ac004cfe7719378730740bf57d958b00baa1ebd2
MeetYourCreator <ramizackshamir@icloud.com>
2021-04-25 19:48:37 -0400

    fotHandler functional for DigitalAlarmClock component, fontHandler sets state

--- components/DigitalClockNumberFontModal.jsx
+++ components/DigitalClockNumberFontModal.jsx
@@ -10,6 +10,36 @@ import { Entypo } from "@expo/vector-icons"
 
 const DigitalClockNumberFontModal = ({ iconName, iconColor, onFontHandle }) => {
   const [showModal, setShowModal] = useState(false)
+  const [_nosifer, set_nosifer] = useState("nosifer")
+  const [_audiowide, set_audiowide] = useState("audiowide")
+  const [_codystarRegular, set_codystarRegular] = useState("codystar-regular")
+  const [_pressStart2p, set_pressStart2p] = useState("press-start-2p")
+  const [_monoton, set_monoton] = useState("monoton")
+  const [_diplomatica, set_diplomatica] = useState("diplomatica")
+
+  const handleSet_nosifer = (event) => {
+    set_nosifer(event)
+  }
+
+  const handleSet_audiowide = (event) => {
+    set_audiowide(event)
+  }
+
+  const handleSet_codystarRegular = (event) => {
+    set_codystarRegular(event)
+  }
+
+  const handleSet_pressStart2p = (event) => {
+    set_pressStart2p(event)
+  }
+
+  const handleSet_monoton = (event) => {
+    set_monoton(event)
+  }
+
+  const handleSet_diplomatica = (event) => {
+    set_diplomatica(event)
+  }
 
   return (
     <SafeAreaView style={styles.wrapper}>
@@ -30,32 +60,38 @@ const DigitalClockNumberFontModal = ({ iconName, iconColor, onFontHandle }) => {
               <MenuFontButton
                 customFont="nosifer"
                 fontDisplay="123"
-                onPress={() => onFontHandle()}
+                onChange={handleSet_nosifer}
+                onPress={() => onFontHandle(_nosifer)}
               />
               <MenuFontButton
                 customFont="audiowide"
                 fontDisplay="123"
-                onPress={() => onFontHandle()}
+                onChange={handleSet_audiowide}
+                onPress={() => onFontHandle(_audiowide)}
               />
               <MenuFontButton
                 customFont="codystar-regular"
                 fontDisplay="123"
-                onPress={() => onFontHandle()}
+                onChange={handleSet_codystarRegular}
+                onPress={() => onFontHandle(_codystarRegular)}
               />
               <MenuFontButton
                 customFont="press-start-2p"
                 fontDisplay="123"
-                onPress={() => onFontHandle()}
+                onChange={handleSet_pressStart2p}
+                onPress={() => onFontHandle(_pressStart2p)}
               />
               <MenuFontButton
                 customFont="monoton"
                 fontDisplay="123"
-                onPress={() => onFontHandle()}
+                onChange={handleSet_monoton}
+                onPress={() => onFontHandle(_monoton)}
               />
               <MenuFontButton
                 customFont="diplomatica"
                 fontDisplay="123"
-                onPress={() => onFontHandle()}
+                onChange={handleSet_diplomatica}
+                onPress={() => onFontHandle(_diplomatica)}
               />
             </View>
             <TouchableOpacity
