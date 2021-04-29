f306f9f8a88669148099d12b7b98ce657b14fa88
MeetYourCreator <ramizackshamir@icloud.com>
2021-04-28 22:41:41 -0400

    init Alarm coomponent

--- components/Alarm.jsx
+++ components/Alarm.jsx
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import { View, SafeAreaView, StyleSheet, Text, TextInput } from 'react-native';
 
 const Alarm = () => {
-
-
-}
+  const [inputMilli, setInputMilli] = useState('');
+  const [inputSec, setInputSec] = useState('');
+  const [inputMin, setInputMin] = useState('');
+  const [inputHour, setInputHour] = useState('');
+};
 
 export default Alarm;
