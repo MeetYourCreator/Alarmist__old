34571282d7b8eeeb15780b56322d34b4ea5e0d56
MeetYourCreator <ramizackshamir@icloud.com>
2021-04-29 22:04:29 -0400

    import Alarm into DigitalClock component

--- components/Alarm.jsx
+++ components/Alarm.jsx
@@ -3,12 +3,21 @@ import { View, SafeAreaView, StyleSheet, Text, TextInput } from 'react-native';
 import AlarmBeep from '../assets/sounds/alarm-beep.mp3';
 
 const Alarm = () => {
-  const [inputTime, setInputTime] = useState('')
+  const [inputTime, setInputTime] = useState('');
 
-  const alarmSound = new Audio();
-  alarmSound.src = {AlarmBeep};
+  const alarmSound = { AlarmBeep };
 
+  const handleInput = (event) => {
+    setInputTime(event.target.value);
+  };
 
+  return (
+    <>
+      <View>
+        <TextInput type='number' value={inputTime} onChange={handleInput}></TextInput>
+      </View>
+    </>
+  );
 };
 
 export default Alarm;
--- screens/DigitalClock/LocalDigitalClock.jsx
+++ screens/DigitalClock/LocalDigitalClock.jsx
@@ -1,34 +1,35 @@
-import React, { useEffect, useState } from "react"
-import { View, SafeAreaView, StyleSheet } from "react-native"
-import { showLocalTime } from "../../services/time.js"
-import { DigitalClockContainer } from "../../components/styled/digital/DigitalClockContainer.js"
-import { Clock } from "../../components/styled/digital/Clock.js"
-import { ClockText } from "../../components/styled/digital/ClockText.js"
-import { Location } from "../../components/styled/digital/Location.js"
+import React, { useEffect, useState } from 'react';
+import { View, SafeAreaView, StyleSheet } from 'react-native';
+import { showLocalTime } from '../../services/time.js';
+import { DigitalClockContainer } from '../../components/styled/digital/DigitalClockContainer.js';
+import { Clock } from '../../components/styled/digital/Clock.js';
+import { ClockText } from '../../components/styled/digital/ClockText.js';
+import { Location } from '../../components/styled/digital/Location.js';
 
-import DigitalClockNumberColorModal from "../../components/DigitalClockNumberColorModal.jsx"
-import DigitalClockNumberFontModal from "../../components/DigitalClockNumberFontModal.jsx"
+import DigitalClockNumberColorModal from '../../components/DigitalClockNumberColorModal.jsx';
+import DigitalClockNumberFontModal from '../../components/DigitalClockNumberFontModal.jsx';
+import Alarm from '../../components/Alarm.jsx';
 
 const LocalDigitalClock = () => {
-  const [localTime, setLocalTime] = useState(showLocalTime())
-  const [colorValue, setColorValue] = useState("#0000FF")
-  const [fontValue, setFontValue] = useState("press-start-2p")
+  const [localTime, setLocalTime] = useState(showLocalTime());
+  const [colorValue, setColorValue] = useState('#0000FF');
+  const [fontValue, setFontValue] = useState('press-start-2p');
 
   const colorHandler = (event) => {
-    setColorValue(event)
-  }
+    setColorValue(event);
+  };
 
-  const fontHandler = (event) => setFontValue(event)
+  const fontHandler = (event) => setFontValue(event);
 
-  const currentColor = colorValue
-  const currentFont = fontValue
+  const currentColor = colorValue;
+  const currentFont = fontValue;
 
   useEffect(() => {
     const timeout = setTimeout(() => {
-      setLocalTime(showLocalTime())
-    }, 1000)
-    return () => clearTimeout(timeout)
-  })
+      setLocalTime(showLocalTime());
+    }, 1000);
+    return () => clearTimeout(timeout);
+  });
 
   return (
     <>
@@ -52,6 +53,9 @@ const LocalDigitalClock = () => {
             <Location style={styles.location}>LocalTime</Location>
           </DigitalClockContainer>
         </View>
+        <View style={styles.alarmInput}>
+          <Alarm />
+        </View>
         <View style={styles.menu}>
           <DigitalClockNumberColorModal
             iconName="color-palette-sharp"
@@ -66,42 +70,42 @@ const LocalDigitalClock = () => {
         </View>
       </SafeAreaView>
     </>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   screenContainer: {
     flex: 1,
-    justifyContent: "space-between",
+    justifyContent: 'space-between',
   },
   clockContainer: {
-    display: "flex",
+    display: 'flex',
     flex: 1,
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
     padding: 40,
     marginTop: 180,
     marginBottom: 90,
   },
   clock: {
-    position: "relative",
+    position: 'relative',
     width: 350,
     height: 100,
     borderWidth: 8,
-    borderColor: "#2d2d2d",
+    borderColor: '#2d2d2d',
     borderRadius: 15,
-    backgroundColor: "#151617",
+    backgroundColor: '#151617',
   },
   clockText: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: "auto",
-    marginBottom: "auto",
-    textAlign: "center",
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 'auto',
+    marginBottom: 'auto',
+    textAlign: 'center',
     fontSize: 35,
-    textShadowColor: "#f70300",
+    textShadowColor: '#f70300',
   },
   location: {
     marginTop: 15,
@@ -109,16 +113,22 @@ const styles = StyleSheet.create({
     marginBottom: 5,
     marginLeft: 7,
     borderTopWidth: 2,
-    textAlign: "center",
-    justifyContent: "center",
-    alignItems: "center",
-    display: "flex"
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    display: 'flex',
+  },
+  alarmInput: {
+    width: 300,
+    backgroundColor: 'blue',
+    borderWidth: 2,
+    borderColor: 'black'
   },
   menu: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    alignItems: "flex-end",
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'flex-end',
     marginTop: 0,
     marginLeft: 20,
     marginRight: 20,
@@ -126,8 +136,8 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: 'center',
   },
-})
+});
 
 export default LocalDigitalClock;
