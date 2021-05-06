0683df3dc7aee5b81831c07d2297390e31a6fb6e
MeetYourCreator <ramizackshamir@icloud.com>
2021-05-06 13:08:42 -0400

    Update styling for Hand and MinuteHand position in AnalogClock component

--- components/styled/analog/MinuteHand.js
+++ components/styled/analog/MinuteHand.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const MinuteHand = styled.View`
   bottom: 38%;
-  left: 45%;
+  left: 40%;
   width: 9px;
   height: 40%;
   background-color: ${(props) => props.color ? props.color : 'black'};
