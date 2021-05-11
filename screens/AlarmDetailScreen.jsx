ff2faf39020099fea1adeaa330d788f52ca1ad47
MeetYourCreator <ramizackshamir@icloud.com>
2021-05-11 17:30:10 -0400

    Refactor AlarmModal

--- screens/AlarmDetailScreen.jsx
+++ screens/AlarmDetailScreen.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import { TouchableOpacity, Text, View, SafeAreaView, StyleSheet } from 'react-native';
+import GenericButton from '../components/GenericButton.jsx'
 
 const AlarmDetailScreen = () => {
   return (
-  
-    <>
-      <View style={styles.alarmDetailContainer}>
-        <Text>Hello</Text>
+    <View>
+      <View style={styles.topButtonsContainer}>
+        <GenericButton
+          backgroundColor="transparent"
+          color="black"
+          borderRadius="1px"
+          borderWidth="0px"
+          width="60px"
+          height="40px"
+          value="Cancel"
+          fontFamily="audiowide"
+        />
+        <GenericButton
+          backgroundColor="transparent"
+          color="black"
+          borderRadius="1px"
+          borderWidth="0px"
+          width="40px"
+          height="40px"
+          value="Save"
+          fontFamily="audiowide"
+        ></GenericButton>
       </View>
-    </>
-  )
+      <View style={styles.input}>
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
