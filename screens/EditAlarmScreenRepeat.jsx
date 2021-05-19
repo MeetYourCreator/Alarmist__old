89fba45171fc758d87afe15028af28e0506ec4a7
MeetYourCreator <ramizackshamir@icloud.com>
2021-05-19 12:26:23 -0400

    Refactor route.params?.post for specific screens, i.e. route.params?.label, route.params?.repeat, etc

--- screens/EditAlarmScreenRepeat.jsx
+++ screens/EditAlarmScreenRepeat.jsx
@@ -3,18 +3,27 @@ import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import CheckSelectRadioButton from '../components/buttons/CheckSelectRadioButton.jsx';
 
-const EditAlarmScreenRepeat = ({ navigation}) => {
-  
-
+const EditAlarmScreenRepeat = ({ navigation, value}) => {
+  const [dayOfWeek, setDayOfWeek] = useState('')
   return (
     <View style={styles.radioButtonsContainer}>
-      <CheckSelectRadioButton day='Monday'></CheckSelectRadioButton>
-      <CheckSelectRadioButton day='Tuesday'></CheckSelectRadioButton>
-      <CheckSelectRadioButton day='Wednesday'></CheckSelectRadioButton>
-      <CheckSelectRadioButton day='Thursday'></CheckSelectRadioButton>
-      <CheckSelectRadioButton day='Friday'></CheckSelectRadioButton>
-      <CheckSelectRadioButton day='Saturday'></CheckSelectRadioButton>
-      <CheckSelectRadioButton day='Sunday'></CheckSelectRadioButton>
+      <CheckSelectRadioButton
+        onPress={() => {
+          navigation.navigate({
+            name: 'Edit Alarm Screen Name',
+            params: { repeat: dayOfWeek },
+            merge: true,
+          });
+        }}
+        dayOfWeek={value}
+        day="Monday"
+      ></CheckSelectRadioButton>
+      <CheckSelectRadioButton day="Tuesday"></CheckSelectRadioButton>
+      <CheckSelectRadioButton day="Wednesday"></CheckSelectRadioButton>
+      <CheckSelectRadioButton day="Thursday"></CheckSelectRadioButton>
+      <CheckSelectRadioButton day="Friday"></CheckSelectRadioButton>
+      <CheckSelectRadioButton day="Saturday"></CheckSelectRadioButton>
+      <CheckSelectRadioButton day="Sunday"></CheckSelectRadioButton>
     </View>
   );
 };
