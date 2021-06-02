cd7be9875f4cad6c382f953db78bc3b5d4055656
MeetYourCreator <ramizackshamir@icloud.com>
2021-06-01 22:08:55 -0400

    Rename files in actions and reducers directory to index.js

--- store/actions/index.js
+++ store/actions/index.js
@@ -1,5 +1,11 @@
+//Action Creator
+
 export const SET_ALARM = 'SET_ALARM'
 
 export const setAlarm = (id) => {
-  return {type: SET_ALARM, alarmId: id}
+  //Return an action
+  return {
+    type: SET_ALARM,
+    alarmId: id
+  }
 }
