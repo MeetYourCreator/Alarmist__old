d91248e7bde87278bd57afe4fb4ef9068a506757
MeetYourCreator <ramizackshamir@icloud.com>
2021-06-08 15:24:42 -0400

    Update actions

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,3 +1,20 @@
 import { createAlarm } from './alarms/creators.js'
+import { setAlarmHour } from './alarms/creators.js'
+import { setAlarmMinutes } from './alarms/creators.js'
+import { setAlarmLabel } from './alarms/creators.js'
+import { setAlarmSound } from './alarms/creators.js'
+import { setAlarmRepeat } from './alarms/creators.js'
+import { setAlarmSnooze } from './alarms/creators.js'
+import { setAlarmAsActive } from './alarms/creators.js'
+import { deleteAlarm } from './alarms/creators.js'
 
-export default createAlarm;
+
+export const createAlarm;
+export const setAlarmHour;
+export const setAlarmMinutes;
+export const setAlarmLabel;
+export const setAlarmSound;
+export const setAlarmRepeat;
+export const setAlarmSnooze;
+export const setAlarmAsActive;
+export const deleteAlarm;
