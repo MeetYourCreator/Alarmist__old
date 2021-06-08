818f85446e799a8948d02e963d79dfbc14d51896
MeetYourCreator <ramizackshamir@icloud.com>
2021-06-08 10:41:26 -0400

    Refactor reducer

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,6 +1,7 @@
 import {ALARMS} from '../data/alarms.js'
-import { SET_ALARM } from '../actions'
+import { CREATE_ALARM } from '../actions'
 
+import { v4 as uuid4 } from uuid;
 //Write the logic for setting and managing alarms
 
 const initialState = {
@@ -11,15 +12,23 @@ const initialState = {
 
 const alarmsReducer = (state = initialState, action) => {
   switch (action.type) {
-    case SET_ALARM:
-      const existingAlarmsIndex = state.alarms.findIndex(alarm => alarm.id === action.alarmId)
-      if (existingAlarmsIndex >= 0) {
-        const existingAlarms = [...state.alarms];
-        return existingAlarms;
-      } else {
-        const newAlarm = state.alarms.find(alarm => alarm.id === action.alarmId)
-        return {...state, alarms: state.alarms.concat(newAlarm)}
-      }
+    case CREATE_ALARM:
+      let newAlarm = {};
+      newAlarm.id = uuid4();
+      console.log('newAlarm:' + newAlarm)
+      return [
+        ...state,
+        state.concat(newAlarm)
+      ]
+    // case SET_ALARM:
+    //   const existingAlarmsIndex = state.alarms.findIndex(alarm => alarm.id === action.alarmId)
+    //   if (existingAlarmsIndex >= 0) {
+    //     const existingAlarms = [...state.alarms];
+    //     return existingAlarms;
+    //   } else {
+    //     const newAlarm = state.alarms.find(alarm => alarm.id === action.alarmId)
+    //     return {...state, alarms: state.alarms.concat(newAlarm)}
+    //   }
     default:
       return state;
   }
