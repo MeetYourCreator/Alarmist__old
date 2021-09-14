89e66a7682b6b29525fade4bc9efddd1ceabd928
MeetYourCreator <ramizackshamir@icloud.com>
2021-09-14 12:18:10 -0400

    Init KeyboardAvoidingWrapper component

---
+++ components/KeyboardAvoidingWrapper.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { KeyboardAvoidingView, ScrollView, TouchableWithoutFeedback, Keyboard } from 'react-native';
+
+const KeyboardAvoidingWrapper = () => {
+
+  return (
+      <>
+      </>
+  )
+}
